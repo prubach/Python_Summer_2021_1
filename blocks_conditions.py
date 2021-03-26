8e39e5fd2adf82bf1431d3647d1a5b6fa4ace8d7
Pawel Rubach <pawel.rubach@sgh.waw.pl>
2021-03-26 13:15:28 +0100

    Blocks and conditions

--- blocks_conditions.py
+++ blocks_conditions.py
@@ -5,7 +5,7 @@ def sum_numbers(aa, bb):
     return aa+bb
 
 
-a = 50
+a = 30
 b = True
 
 print(type(b))
@@ -20,3 +20,29 @@ print(c)
 print(sum_numbers(58, 90))
 print('a=' + str(a))
 #print(cc)
+
+
+if a>=40 and b:
+    print('a>=40 and b is True')
+
+d = 100 if not a>40 else -100
+print(d)
+
+b1 = True
+b2 = True
+
+''' 
+mulit line 
+comment
+
+'''
+
+# XOR
+if b1 ^ b2:
+    print('xor on b1 and b2 is True')
+else:
+    print('xor is not true')
+
+# Rest of the division
+c = 10 % 3
+print(c)
