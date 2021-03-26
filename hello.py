97dd4537d5967aebdf58ec2de4c101d84ad99fa1
Pawel Rubach <pawel.rubach@sgh.waw.pl>
2021-03-26 12:43:14 +0100

    Testing variables

--- hello.py
+++ hello.py
@@ -2,3 +2,67 @@ print('Hello')
 if True:
     print('in if')
     print('abc')
+
+a = 10
+b = 20
+a = a + 5.5
+a = -57027627602360732679679832679877295729857927597593275973597295793275329865937529725973259795287296721067230673206719671067106739867309267989839693798
+c = a + b
+print(a)
+print(c)
+print(type(a))
+
+f = 25252523651215.252525215235252525
+print(f)
+print(type(f))
+
+g = 0.000000000000000000000000000000252525234378439687396739627096203673679651215252525215235252525
+print(g)
+print(type(g))
+
+s = 'abcdef'
+
+print(s)
+print(type(s))
+print(len(s))
+
+
+s2 = s + str(b)
+print(s2)
+print(s.isalnum())
+print('3535'.isalnum())
+
+print(s.index('e'))
+print(s[1:])
+print(s[1:3])
+print(s[:2])
+print(s[-2:])
+
+if type(s) == type(s2):
+    print('Types of s and s2 are the same')
+
+t = None
+print(type(t))
+
+
+
+if t:
+    print('t is true')
+else:
+    print('t not true')
+
+
+
+
+
+s3 = ' ab   c'
+print(s3)
+s3 = s3.strip()
+print(s3)
+
+if s3:
+    print('s3 is true')
+else:
+    print('s3 not true')
+
+
