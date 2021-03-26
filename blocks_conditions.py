a734544e844f04e77f10d1f47f3a99a5ed447341
Pawel Rubach <pawel.rubach@sgh.waw.pl>
2021-03-26 13:03:30 +0100

    Blocks and conditions

---
+++ blocks_conditions.py
@@ -0,0 +1,22 @@
+
+def sum_numbers(aa, bb):
+    print('aa inside sum=' + str(aa))
+    cc = 646
+    return aa+bb
+
+
+a = 50
+b = True
+
+print(type(b))
+
+if b:
+    print('b is True')
+    c = 60
+    print('a=' + str(a))
+    print('c=' + str(c))
+
+print(c)
+print(sum_numbers(58, 90))
+print('a=' + str(a))
+#print(cc)
