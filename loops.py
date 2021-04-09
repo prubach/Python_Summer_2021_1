5ae6860f577f2961edfcc1cb847f27050a3fd125
Paweł Rubach <pawel.rubach@gmail.com>
2021-04-09 13:01:51 +0200

    Numbers and loops

---
+++ loops.py
@@ -0,0 +1,22 @@
+
+for i in range(2, 8):
+    if i==3:
+        continue
+    if i==5:
+        break
+    print(i)
+
+i = 0
+while i < 10:
+    print(i)
+    #i = i + 1
+    i += 1
+
+print()
+c = 10
+
+b = 10 if c > 50 else 20
+print(b)
+#for i in range(2, 5):
+#    print(i)
+
--- num_vars.py
+++ num_vars.py
@@ -2,3 +2,10 @@ a = 0b101
 print(a)
 
 print(0x2e)
+
+
+b = 1.79e308
+c = 1.8e308
+
+print(b)
+print(c)
