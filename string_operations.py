38a2b0d46d0accb6584cd928cabbb1922c7b2d98
Paweł Rubach <pawel.rubach@gmail.com>
2021-04-09 12:41:47 +0200

    String operations

---
+++ string_operations.py
@@ -0,0 +1,19 @@
+print('3535'.isalpha())
+print('abcc'.isalpha())
+
+print('3535'.isdigit())
+print('abcc'.isdigit())
+
+print(ord('B'))
+print(ord('ś'))
+
+print('łóść„ąœdfgd')
+
+print('abc\ndef\negwgwgwg')
+print('\tabca \tgw\gewgewg\t\tagag')
+s = "asagabsbas'g"
+s = 'asagabsbas\'gt\\tt'
+print(s)
+a = '   agagbsb '
+print(a.strip())
+
