f7fa5f7b771580ffb326de78a77a53619994ba68
Paweł Rubach <pawel.rubach@sorcersoft.com>
2021-04-16 12:53:50 +0200

    Summing Lists

--- lists_dict_tuples.py
+++ lists_dict_tuples.py
@@ -31,5 +31,38 @@ print(l2[2][2:])
 
 l5 = [[1, 6, 8], [8, 9, 3], [3, 9, 4]]
 # sum rows, sum cols and sum of all
+for row in l5:
+    print(row)
+    for el in row:
+        print(el)
 
+print('----------------')
+print(l5[0][1])
 
+l6 = [0, 0, 0]
+for j in range(len(l5)):
+    for k in range(len(l5[j])):
+        l6[k] += l5[j][k]
+print(l6)
+
+sum_all = 0
+for row in l5:
+    #sum_all += sum(row)
+    sum_all = sum_all + sum(row)
+print(sum_all)
+#print(len(set(l4)))
+
+import numpy as np
+
+nl5 = np.array(l5)
+print(nl5)
+#print('Sum of elements: ' + str(np.sum(l5)))
+print('Sum of elements: %i' % (np.sum(l5)))
+print('Sum of rows: ' + str(np.sum(l5, axis=1)))
+print('Sum of cols: ' + str(np.sum(l5, axis=0)))
+
+f = 45346.6326525225
+print('My float num: %.8f' % f)
+
+s = 'My float num: %.8f' % f
+print(s)
