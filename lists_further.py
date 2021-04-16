3728e110dc3ee277d169916d133f3aaed72b8889
Paweł Rubach <pawel.rubach@sorcersoft.com>
2021-04-16 13:06:44 +0200

    dicts and tuples

---
+++ lists_further.py
@@ -0,0 +1,31 @@
+l3 = [90825, 5625, 5625, 63, 909]
+l3.insert(1, 400)
+
+l4 = []
+for el in l3:
+    l4.append(el*2)
+
+print(l4)
+
+l5 = [el*2 for el in l3]
+print(l5)
+
+s1 = set(l5)
+print(s1)
+
+t1 = (14, 25)
+print(t1)
+print(t1[0])
+# tuple is immutable - you can't change it
+#t1[0] = 5252
+a, b = (365, 3636)
+print(a)
+print(b)
+
+d1 = {'a' : 'abcva', 'b': 'sggsgb', 'c': 'def'}
+print(d1['b'])
+
+for k in d1.keys():
+    print('Key: %s Value: %s' % (k, d1[k]))
+
+print(list(d1.values()))
