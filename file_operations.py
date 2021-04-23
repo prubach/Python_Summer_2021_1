f264273d8b6b211c71fb53d9bfe56742fd44f12e
Paweł Rubach <pawel.rubach@sorcersoft.com>
2021-04-23 12:17:32 +0200

    Writing to json

--- file_operations.py
+++ file_operations.py
@@ -1,4 +1,6 @@
 days_d = {1: 'Monday', 2: 'Tuesday', 7: 'Sunday'}
+
+my_list = [3425, 25252, 46778, 4]
 print(str(days_d))
 f = 'out_days.txt'
 with open(f, 'w') as my_file:
@@ -6,5 +8,18 @@ with open(f, 'w') as my_file:
 
 import json
 f = 'out_days.json'
-with open(f, 'w') as my_file:
-    json.dump(days_d, f)
+#with open(f, 'w') as my_file:
+#    json.dump(my_list, my_file)
+    #my_file.
+# alternative way instead of 'with'
+my_file = open(f, 'w')
+json.dump(my_list, my_file)
+my_file.close()
+
+
+with open(f, 'r') as my_file:
+    out_days_d = json.load(my_file)
+    #print(out_days_d['1'])
+    print(out_days_d)
+    print(type(out_days_d))
+    print(sum(out_days_d))
