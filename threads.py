2a75da3c162a8d7a5c35d2da504bba0453b602a9
Pawel Rubach <pawel.rubach@gmail.com>
2021-06-04 12:06:56 +0200

    Threads - running sequentially

---
+++ threads.py
@@ -0,0 +1,26 @@
+import threading
+import time
+
+class MyThread(threading.Thread):
+    def __init__(self, id, name, delay, counter):
+        self.id = id
+        self.name = name
+        self.counter = counter
+        self.delay = delay
+
+    def run(self):
+        print('starting thread: ' + self.name)
+        while self.counter > 0:
+            time.sleep(self.delay)
+            print('{}: {}'.format(self.name, time.ctime(time.time())))
+            self.counter -= 1
+        print('finished thread: ' + self.name)
+
+
+th1 = MyThread(1, 'th1', 1, 5)
+th2 = MyThread(2, 'th2', 1, 5)
+th3 = MyThread(3, 'th3', 1, 5)
+
+th1.run()
+th2.run()
+th3.run()
