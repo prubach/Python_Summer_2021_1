36317575073f0a306289829089b048c8ecc46846
Paweł Rubach <pawel.rubach@sorcersoft.com>
2021-06-11 12:16:13 +0200

    Create thumbnails of images using multiprocessing

--- threads_download.py
+++ threads_download.py
@@ -1,9 +1,13 @@
+import multiprocessing
+from pathlib import Path
 from queue import Queue
 from threading import Thread
 from multiprocessing.pool import Pool
+from concurrent.futures.process import ProcessPoolExecutor
+
 from functools import partial
 
-from knotprot_download import time_it, download_link, setup_download_dir, get_proteins
+from knotprot_download import time_it, download_link, setup_download_dir, get_proteins, create_thumbnail
 
 def run_sequentially(dir):
     for p in get_proteins():
@@ -42,6 +46,18 @@ def run_multiprocessing(mydir):
     with Pool(16) as pool:
         pool.map(download, proteins)
 
+### Thumbnails
+def thumbnails_sequential():
+    for image_path in Path('images').iterdir():
+        print(image_path)
+        create_thumbnail((192, 192), image_path)
+
+def thumbnails_parallel():
+    cr_thumb = partial(create_thumbnail, (192, 192))
+    with ProcessPoolExecutor(max_workers=4) as executor:
+        executor.map(cr_thumb, Path('images').iterdir())
+
+
 mydir = setup_download_dir()
 # Sequential
 #time_it(run_sequentially, mydir)
@@ -49,3 +65,6 @@ mydir = setup_download_dir()
 #time_it(run_workers, mydir)
 # Multiprocessing
 time_it(run_multiprocessing, mydir)
+# Thumbnails
+#time_it(thumbnails_sequential)
+time_it(thumbnails_parallel)
